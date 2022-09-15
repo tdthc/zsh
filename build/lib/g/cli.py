ab8ab91acd9c9eb0316f7ad041837c52c9f071ab
Trần Đăng Tuấn <tuan.tran.9852@gmail.com>
2022-09-15 23:47:19 +0700

    ad

---
+++ build/lib/g/cli.py
@@ -0,0 +1,20 @@
+import click
+import os
+
+@click.command()
+@click.argument("alias")
+def main(alias):
+    git = dict(
+        g='git',
+        gi="git init",
+        gaa='git add --all',
+        gst='git status',
+        gp='git push'
+    )
+    os.system(git.get(alias))
+
+def start():
+    main(obj={})
+
+if __name__ == "__main__":
+    start()
---
+++ v/cli.py
@@ -0,0 +1,25 @@
+import click
+import os
+
+@click.command()
+@click.argument("alias")
+def main(alias):
+    git = dict(
+        g='git',
+        gi="git init",
+        gaa='git add --all',
+        gst='git status',
+        gp='git push',
+        gl='git pull',
+        glo='git log --oneline --decorate',
+        ghh='git help',
+        gcpa='git cherry-pick --abort',
+        gcpc='git cherry-pick --continue'
+    )
+    os.system(git.get(alias))
+
+def start():
+    main(obj={})
+
+if __name__ == "__main__":
+    start()
