cfe5f1fec64cbae4760b34603ba3299c0ffd93a0
Trần Đăng Tuấn <tuan.tran.9852@gmail.com>
2022-10-07 00:01:02 +0700

    da

--- Alias.bat
+++ Alias.bat
@@ -3,7 +3,12 @@ DOSKEY ls=DIR $*
 
 DOSKEY ga=git add
 DOSKEY gst=git status
+DOSKEY gco=git checkout
 DOSKEY gaa=git add --all
+DOSKEY gcmsg=git commit -m
+DOSKEY gcp=git cherry-pick
+DOSKEY gcpc=git cherry-pick --continue
+DOSKEY gcpa=git cherry-pick --abort
 DOSKEY gapa=git add --patch
 DOSKEY gau=git add --update
 DOSKEY gav=git add --verbose
